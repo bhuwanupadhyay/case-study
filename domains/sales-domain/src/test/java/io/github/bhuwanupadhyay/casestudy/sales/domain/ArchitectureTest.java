db3c519f6db832048026428aab05c10755a2ff84
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-09-27 10:15:36 +0545

    chore(wip): define domain structure

--- domains/sales-domain/src/test/java/io/github/bhuwanupadhyay/casestudy/sales/domain/ArchitectureTest.java
+++ domains/sales-domain/src/test/java/io/github/bhuwanupadhyay/casestudy/sales/domain/ArchitectureTest.java
@@ -1,61 +1,17 @@
 package io.github.bhuwanupadhyay.casestudy.sales.domain;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchRules;
 import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.CompositeArchRule;
-import com.tngtech.archunit.library.Architectures;
-import org.slf4j.Logger;
-
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-import static com.tngtech.archunit.library.GeneralCodingRules.*;
+import io.github.bhuwanupadhyay.casestudy.archunit.rules.BasicRules;
+import io.github.bhuwanupadhyay.casestudy.archunit.rules.DomainRules;
 
 @AnalyzeClasses(packages = ArchitectureTest.PACKAGE)
 public class ArchitectureTest {
 
   public static final String PACKAGE = "io.github.bhuwanupadhyay.casestudy.sales";
   @ArchTest
-  public final ArchRule domain_dependencies_are_respected = Architectures.layeredArchitecture()
-      .layer("Domain Commands").definedBy(PACKAGE +"." + "domain.commands..")
-      .layer("Domain Aggregates").definedBy(PACKAGE +"." + "domain.model.aggregates.")
-      .layer("Domain Entities").definedBy(PACKAGE +"." + "domain.model.entities..")
-      .layer("Domain Events").definedBy(PACKAGE + "domain.model.events.")
-      .layer("Domain Repositories").definedBy(PACKAGE +"." + "domain.model.repositories..")
-      .layer("Domain ValueObjects").definedBy(PACKAGE +"." + "domain.model.valueobjects..")
-      .layer("Domain Services").definedBy(PACKAGE +"." + "domain.services..")
-      .whereLayer("Domain Services").mayNotBeAccessedByAnyLayer()
-      .whereLayer("Domain Repositories").mayNotBeAccessedByAnyLayer()
-      .whereLayer("Domain Commands").mayOnlyBeAccessedByLayers("Domain Services")
-      .whereLayer("Domain Aggregates").mayOnlyBeAccessedByLayers("Domain Repositories", "Domain Services")
-      .whereLayer("Domain Entities").mayOnlyBeAccessedByLayers("Domain Aggregates", "Domain Services")
-      .whereLayer("Domain ValueObjects").mayOnlyBeAccessedByLayers("Domain Aggregates", "Domain Entities", "Domain Services", "Domain Events")
-      .whereLayer("Domain Events").mayOnlyBeAccessedByLayers("Domain Aggregates");
+  private final ArchRules basic_rules_are_respected = ArchRules.in(BasicRules.class);
   @ArchTest
-  private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
-  @ArchTest
-  private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
-  @ArchTest
-  private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
-  @ArchTest
-  private final ArchRule loggers_should_be_private_static_final =
-      fields().that().haveRawType(Logger.class)
-          .should().bePrivate()
-          .andShould().beStatic()
-          .andShould().beFinal()
-          .because("we agreed on this convention");
-  @ArchTest
-  private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
-  @ArchTest
-  private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
-  @ArchTest
-  private final ArchRule no_classes_should_access_standard_streams_or_throw_generic_exceptions =
-      CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS)
-          .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS);
-
-  @ArchTest
-  private void no_access_to_standard_streams_as_method(JavaClasses classes) {
-    noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
-  }
+  private final ArchRules dependencies_are_respected = ArchRules.in(DomainRules.class);
 }
--- plugins/archunit-code-qualtity-rules/src/main/java/io/github/bhuwanupadhyay/casestudy/archunit/rules/DomainRules.java
+++ plugins/archunit-code-qualtity-rules/src/main/java/io/github/bhuwanupadhyay/casestudy/archunit/rules/DomainRules.java
@@ -2,43 +2,46 @@ package io.github.bhuwanupadhyay.casestudy.archunit.rules;
 
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.library.Architectures;
-import com.tngtech.archunit.library.GeneralCodingRules;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 
 public class DomainRules {
+  
+  public static final String REPOSITORIES = "..domain.model.repositories..";
+  public static final String AGGREGATES = "..domain.model.aggregates..";
+  public static final String ENTITIES = "..domain.model.entities..";
+  public static final String EVENTS = "..domain.model.events..";
+  public static final String VALUEOBJECTS = "..domain.model.valueobjects..";
+  public static final String COMMANDS = "..domain.commands..";
+  public static final String SERVICES = "..domain.services..";
 
   @ArchTest
-  public static final ArchRule ruleOne =
-      GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+  private final ArchRule domain_commands_are_respected = ArchRuleDefinition.classes()
+      .that().resideInAPackage(COMMANDS)
+      .should().onlyBeAccessed()
+      .byAnyPackage(SERVICES);
 
   @ArchTest
-  public static final ArchRule LAYERED_ARCHITECTURE = Architectures.layeredArchitecture()
-      .layer("Commands")
-      .definedBy(".commands.")
-      .layer("Aggregates")
-      .definedBy(".model.aggregates.")
-      .layer("Entities")
-      .definedBy(".model.entities.")
-      .layer("Events")
-      .definedBy(".model.events.")
-      .layer("Repositories")
-      .definedBy(".model.repositories.")
-      .layer("ValueObjects")
-      .definedBy(".model.valueobjects.")
-      .layer("Services")
-      .definedBy(".services.")
-      .whereLayer("Services")
-      .mayNotBeAccessedByAnyLayer()
-      .whereLayer("Repositories")
-      .mayNotBeAccessedByAnyLayer()
-      .whereLayer("Commands")
-      .mayOnlyBeAccessedByLayers("Services")
-      .whereLayer("Aggregates")
-      .mayOnlyBeAccessedByLayers("Repositories", "Services")
-      .whereLayer("Entities")
-      .mayOnlyBeAccessedByLayers("Aggregates", "Services")
-      .whereLayer("ValueObjects")
-      .mayOnlyBeAccessedByLayers("Aggregates", "Entities", "Services", "Events")
-      .whereLayer("Events")
-      .mayOnlyBeAccessedByLayers("Aggregates");
+  private final ArchRule domain_aggregates_are_respected = ArchRuleDefinition.classes()
+      .that().resideInAPackage(AGGREGATES)
+      .should().onlyBeAccessed()
+      .byAnyPackage(SERVICES, REPOSITORIES);
+
+  @ArchTest
+  private final ArchRule domain_entities_are_respected = ArchRuleDefinition.classes()
+      .that().resideInAPackage(ENTITIES)
+      .should().onlyBeAccessed()
+      .byAnyPackage(SERVICES, AGGREGATES);
+
+  @ArchTest
+  private final ArchRule domain_events_are_respected = ArchRuleDefinition.classes()
+      .that().resideInAPackage(EVENTS)
+      .should().onlyBeAccessed()
+      .byAnyPackage(AGGREGATES);
+
+  @ArchTest
+  private final ArchRule domain_valueobjects_are_respected = ArchRuleDefinition.classes()
+      .that().resideInAPackage(VALUEOBJECTS)
+      .should().onlyBeAccessed()
+      .byAnyPackage(AGGREGATES, ENTITIES, SERVICES,
+          EVENTS);
 }
