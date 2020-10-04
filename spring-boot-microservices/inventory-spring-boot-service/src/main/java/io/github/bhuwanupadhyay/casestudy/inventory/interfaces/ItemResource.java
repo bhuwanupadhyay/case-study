cdbae22f897537087e9198d0685bad9d30ae5a0e
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-10-05 00:08:32 +0545

    chore(wip): inventory cleanup

--- spring-boot-microservices/inventory-spring-boot-service/src/main/java/io/github/bhuwanupadhyay/casestudy/inventory/interfaces/ItemResource.java
+++ spring-boot-microservices/inventory-spring-boot-service/src/main/java/io/github/bhuwanupadhyay/casestudy/inventory/interfaces/ItemResource.java
@@ -1,11 +1,14 @@
 package io.github.bhuwanupadhyay.casestudy.inventory.interfaces;
 
 import io.github.bhuwanupadhyay.casestudy.inventory.infrastructure.repositories.mybatis.dto.ItemDto;
+import java.math.BigDecimal;
 
 public class ItemResource {
 
   private String itemName;
   private String itemDescription;
+  private BigDecimal price;
+  private Integer quantity;
 
   public ItemResource() {
   }
@@ -13,6 +16,8 @@ public class ItemResource {
   public ItemResource(ItemDto itemDto) {
     this.itemName = itemDto.getItemName();
     this.itemDescription = itemDto.getItemDescription();
+    this.price = itemDto.getPrice();
+    this.quantity = itemDto.getQuantity();
   }
 
   public String getItemDescription() {
@@ -21,5 +26,23 @@ public class ItemResource {
 
   public String getItemName() {
     return itemName;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  @Override
+  public String toString() {
+    return "ItemResource{" +
+        "itemName='" + itemName + '\'' +
+        ", itemDescription='" + itemDescription + '\'' +
+        ", price=" + price +
+        ", quantity=" + quantity +
+        '}';
   }
 }
