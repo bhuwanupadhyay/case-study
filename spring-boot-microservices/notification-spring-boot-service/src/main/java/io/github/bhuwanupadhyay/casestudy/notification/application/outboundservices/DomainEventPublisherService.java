6cdf4d0229c76a53a62fb968daf40d3e816307f5
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-10-05 10:04:56 +0545

    chore(wip): notifications

--- spring-boot-microservices/notification-spring-boot-service/src/main/java/io/github/bhuwanupadhyay/casestudy/notification/application/outboundservices/DomainEventPublisherService.java
+++ spring-boot-microservices/notification-spring-boot-service/src/main/java/io/github/bhuwanupadhyay/casestudy/notification/application/outboundservices/DomainEventPublisherService.java
@@ -1,18 +1,12 @@
 package io.github.bhuwanupadhyay.casestudy.notification.application.outboundservices;
 
-import io.github.bhuwanupadhyay.casestudy.billing.domain.model.events.ModificationBilled;
-import io.github.bhuwanupadhyay.casestudy.billing.domain.model.events.OrderBilled;
-import io.github.bhuwanupadhyay.casestudy.billing.domain.model.valueobjects.BillAmount;
-import io.github.bhuwanupadhyay.casestudy.billing.domain.model.valueobjects.BillingId;
-import io.github.bhuwanupadhyay.casestudy.billing.domain.model.valueobjects.OrderId;
-import io.github.bhuwanupadhyay.casestudy.billing.infrastructure.brokers.EventSource;
+import io.github.bhuwanupadhyay.casestudy.notification.infrastructure.brokers.EventSource;
 import io.github.bhuwanupadhyay.ddd.DomainEvent;
 import io.github.bhuwanupadhyay.ddd.DomainEventPublisher;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,28 +21,8 @@ public class DomainEventPublisherService implements DomainEventPublisher {
   @EventListener
   public void publish(DomainEvent domainEvent) {
     Map<String, Object> h = new HashMap<>();
-    h.put("X-Service", "Billing");
+    h.put("X-Service", "Notification");
     h.put("X-DomainEvent", domainEvent.getClass().getName());
     MessageHeaders headers = new MessageHeaders(h);
-
-    if (domainEvent instanceof OrderBilled orderBilled) {
-      Map<String, Object> payload =
-          toPayload(orderBilled.billingId(), orderBilled.orderId(), orderBilled.billAmount());
-      eventSource.orderBilled().send(MessageBuilder.createMessage(payload, headers));
-    } else if (domainEvent instanceof ModificationBilled modificationBilled) {
-      Map<String, Object> payload =
-          toPayload(modificationBilled.billingId(), modificationBilled.orderId(),
-              modificationBilled.billAmount());
-      eventSource.modificationBilled().send(MessageBuilder.createMessage(payload, headers));
-    }
-  }
-
-  private Map<String, Object> toPayload(BillingId billingId, OrderId orderId,
-      BillAmount billAmount) {
-    Map<String, Object> payload = new HashMap<>();
-    payload.put("billingId", billingId.value());
-    payload.put("orderId", orderId.value());
-    payload.put("billAmount", billAmount.amount());
-    return payload;
   }
 }
