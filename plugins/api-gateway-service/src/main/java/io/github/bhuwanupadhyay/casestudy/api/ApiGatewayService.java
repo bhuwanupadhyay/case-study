82ebe899ec3d70c8fc50475c1e8921dab6b66f7d
Bhuwan Prasad Upadhyay <bhuwan.upadhyay@clusus.com>
2020-10-09 10:44:41 +0545

    chore(wip): api gateway service

---
+++ plugins/api-gateway-service/src/main/java/io/github/bhuwanupadhyay/casestudy/api/ApiGatewayService.java
@@ -0,0 +1,12 @@
+package io.github.bhuwanupadhyay.casestudy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGatewayService.class, args);
+  }
+}
